44106fdee8953f3762455c61f19a0cc52b69637b
sizlinx <sizlinx@gmail.com>
2021-01-10 20:26:36 +0900

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tidory Framework
+
+This is a **template** for [Tidory Framework](http://www.tidory.com).
+
+## License
+
+[MIT](https://github.com/tidory/tidory/blob/master/LICENSE)
+
+Copyright 2018-2020. [SangWoo Jeong](https://github.com/pronist). All rights reserved.
---
+++ tidory.config.js
@@ -0,0 +1,33 @@
+/**
+ * Tidory Configuration
+ * https://tidory.com/docs/configuration/
+ */
+module.exports = {
+  /**
+   * Tistory session cookie value
+   */
+  ts_session: null,
+
+  /**
+   * Tistory blog URL
+   */
+  url: null,
+
+  /**
+   * Preview
+   */
+  preview: {
+    /**
+     * Preview Mode
+     *
+     * index
+     * entry
+     * category
+     * tag,
+     * location
+     * media,
+     * guestbook
+     */
+    mode: 'index'
+  }
+}
